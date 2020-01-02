bc652504b7170ef086262c2c499a9fea43ef404c
Alisson Pelizaro <alissonpelizaro@hotmail.com>
2020-01-02 16:25:31 -0300

    Adding linkId on return to getLinks method
    Adding [linkId] on return to getLinks method to better link manipulation

--- jquery.flowchart.js
+++ jquery.flowchart.js
@@ -1121,6 +1121,7 @@ jQuery(function ($) {
                 if (this.data.links.hasOwnProperty(linkId)) {
                     var linkData = this.data.links[linkId];
                     if (linkData.fromOperator === operatorId) {
+                        linkData.linkId = linkId;
                         result.push(linkData);
                     }
                 }
@@ -1136,6 +1137,7 @@ jQuery(function ($) {
                 if (this.data.links.hasOwnProperty(linkId)) {
                     var linkData = this.data.links[linkId];
                     if (linkData.toOperator === operatorId) {
+                        linkData.linkId = linkId;
                         result.push(linkData);
                     }
                 }
